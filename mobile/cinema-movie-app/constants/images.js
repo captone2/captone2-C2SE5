082899aed9d7203adbb2f77846366e97079a4d54
Hồ Văn Huệ <hovanhue2k1@gmail.com>
2023-04-23 21:42:59 +0700

    update

--- mobile/cinema-movie-app/constants/images.js
+++
@@ -1,5 +0,0 @@
-const profile_photo = require("../assets/images/dummy_profile/profile_photo.png");
-
-export default {
-    profile_photo,
-}
--- mobile/cinema-movie-app/constants/index.js
+++
@@ -1,6 +0,0 @@
-import dummyData from "./dummy";
-import icons from "./icons";
-import images from "./images";
-import theme, { SIZES } from "./theme";
-
-export { dummyData, icons, images, theme, SIZES };
